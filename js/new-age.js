9d1e4fcb206d15af464d0f2ea7e2528bf7a22ba3
Malik Inkatha <minkatha01@gmail.com>
2017-02-19 13:44:02 -0600

    Update project for app recommender.

--- js/new-age.js
+++ js/new-age.js
@@ -29,3 +29,49 @@
     })
 
 })(jQuery); // End of use strict
+
+var QueryString = function () {
+    // This function is anonymous, is executed immediately and 
+    // the return value is assigned to QueryString!
+    var queryString = {};
+    var query = window.location.search.substring(1);
+    var variables = query.split('&');
+    for (var i = 0; i < variables.length; i++) {
+        var pair = variables[i].split('=');
+
+        var parameterType = typeof queryString[pair[0]];
+
+        if (parameterType === 'undefined') {
+            queryString[pair[0]] = decodeURIComponent(pair[1]);
+        } else if (parameterType === 'string') {
+            var arr = [ queryString[pair[0]], decodeURIComponent(pair[1]) ]
+            queryString[pair[0]] = arr;
+        } else {
+            queryString[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return queryString;
+}();
+
+var AppRecommender = function () {
+
+    var websiteImage = document.getElementById('recommend-image');
+    console.log(HasParentalControls());
+    if (HasParentalControls() === 'yes') {
+        alert('yes');
+    } else if (HasParentalControls() === 'no') {
+        alert('no');
+    }
+}();
+
+var HasParentalControls = function () {
+    return QueryString.questionOne;
+}
+
+var BlockTextingOrEducate = function () {
+    return QueryString.questionTwo;
+}
+
+var BlockCommunicationOrReduceDistractions = function () {
+    return QueryString.questionThree;
+}
